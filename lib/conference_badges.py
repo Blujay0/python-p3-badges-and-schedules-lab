ed9751fc639f03bd19785549cba5b5b6eb4c68a0
Jay Lim <jlimoman@gmail.com>
2023-05-23 11:35:53 -0600

    complete python-p3-badges-and-schedules-lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,33 @@
 def badge_maker(name):
-    return None
+    return (f"Hello, my name is {name}.")
 
 def batch_badge_creator(names):
-    return None
+    list_of_names = []
+    
+    for name in names:
+        list_of_names.append(f"Hello, my name is {name}.")
+        
+    return list_of_names
 
 def assign_rooms(names):
-    return None
+    list_of_room_assignments = []
+    
+    for name in names:
+        list_of_room_assignments.append(f"Hello, {name}! You'll be assigned to room {names.index(name)+1}!")
+    
+    return list_of_room_assignments
 
 def printer(names):
-    return None
+    # output of batch_badge_creator
+    # output of assign_rooms
+    # loop thru each list
+    batch_list = batch_badge_creator(names)
+    
+    for name in batch_list:
+        print(name)
+        
+    room_list = assign_rooms(names)
+    
+    for string in room_list:
+        print(string)
+    
